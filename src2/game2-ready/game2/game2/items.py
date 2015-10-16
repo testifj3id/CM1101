bc7595f44cfdbe2022a86467938dbe1b39744924
testifj3id <dkaradachki@outlook.com>
2015-10-16 12:20:18 +0100

    new

---
+++ src2/game2-ready/game2/game2/items.py
@@ -0,0 +1,73 @@
+item_id = {
+    "id": "id",
+
+    "name": "id card",
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?...""",
+
+    "weight": "0.1"
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "name": "laptop",
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!",
+
+    "weight": "2.0"
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees.",
+
+    "weight": "1.0"
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "name": "a pack of biscuits",
+
+    "description": "A pack of biscuits.",
+
+    "weight": "0.5"
+}
+
+item_pen = {
+    "id": "pen",
+
+    "name": "a pen",
+
+    "description": "A basic ballpoint pen.",
+
+    "weight": "0.1"
+}
+
+item_handbook = {
+    "id": "handbook",
+
+    "name": "a student handbook",
+
+    "description": "This student handbook explains everything. Seriously.",
+
+    "weight": "0.2"
+}
+
+item_pretty = {
+    "id": "girl",
+
+    "name": "Regine",
+
+    "description": "Hottest girl you've ever seen.Seriously.",
+
+    "weight": "2.9"
+}
---
+++ src2/gameparser.py
@@ -0,0 +1,93 @@
+skip_words = ['a', 'about', 'all', 'an', 'another', 'any', 'around', 'at',
+              'bad', 'beautiful', 'been', 'better', 'big', 'can', 'every', 'for',
+              'from', 'good', 'have', 'her', 'here', 'hers', 'his', 'how',
+              'i', 'if', 'in', 'into', 'is', 'it', 'its', 'large', 'later',
+              'like', 'little', 'main', 'me', 'mine', 'more', 'my', 'now',
+              'of', 'off', 'oh', 'on', 'please', 'small', 'some', 'soon',
+              'that', 'the', 'then', 'this', 'those', 'through', 'till', 'to',
+              'towards', 'until', 'us', 'want', 'we', 'what', 'when', 'why',
+              'wish', 'with', 'would', 'could']
+
+
+def filter_words(words, skip_words):
+    """This function takes a list of words and returns a copy of the list from
+    which all words provided in the list skip_words have been removed.
+    For example:
+
+    >>> filter_words(["help", "me", "please"], ["me", "please"])
+    ['help']
+
+    >>> filter_words(["go", "south"], skip_words)
+    ['go', 'south']
+
+    >>> filter_words(['how', 'about', 'i', 'go', 'through', 'that', 'little', 'passage', 'to', 'the', 'south'], skip_words)
+    ['go', 'passage', 'south']
+
+    """
+    filter_print = [x for x in words if x not in skip_words]
+    #filter_print = list(set(words) & set(skip_words))
+    return filter_print
+
+
+def remove_punct(text):
+    """This function is used to remove all punctuation
+    marks from a string. Spaces do not count as punctuation and should
+    not be removed. The funcion takes a string and returns a new string
+    which does not contain any puctuation. For example:
+
+    >>> remove_punct("Hello, World!")
+    'Hello World'
+    >>> remove_punct("-- ...Hey! -- Yes?!...")
+    ' Hey  Yes'
+    >>> remove_punct(",go!So.?uTh")
+    'goSouTh'
+    """
+    # define punctuation
+    punctuation_chars = '''-[];:!,<>./(){}'"&*_\?~%@#$^'''
+
+    # remove punctuation
+    removed_punct = ""
+    for char in text:
+        if char not in punctuation_chars:
+            removed_punct = removed_punct + char
+
+    # display unpunctuated string
+    return removed_punct
+
+
+def normalise_input(user_input):
+    """This function removes all punctuation from the string and converts it to
+    lower case. It then splits the string into a list of words (also removing
+    any extra spaces between words) and further removes all "unimportant"
+    words from the list of words using the filter_words() function. The
+    resulting list of "important" words is returned. For example:
+
+    >>> normalise_input("  Go   south! ")
+    ['go', 'south']
+    >>> normalise_input("!!!  tAkE,.    LAmp!?! ")
+    ['take', 'lamp']
+    >>> normalise_input("HELP!!!!!!!")
+    ['help']
+    >>> normalise_input("Now, drop the sword please.")
+    ['drop', 'sword']
+    >>> normalise_input("Kill ~ tHe :-  gObLiN,. wiTH my SWORD!!!")
+    ['kill', 'goblin', 'sword']
+    >>> normalise_input("I would like to drop my laptop here.")
+    ['drop', 'laptop']
+    >>> normalise_input("I wish to take this large gem now!")
+    ['take', 'gem']
+    >>> normalise_input("How about I go through that little passage to the south...")
+    ['go', 'passage', 'south']
+
+    """
+    # Remove punctuation and convert to lower case
+    # define punctuation
+    user_input1 = remove_punct(user_input).lower()
+    split_input = user_input1.split()
+    new_input = filter_words(split_input, skip_words)
+    return new_input
+
+
+    #
+    # COMPLETE ME!
+    #
